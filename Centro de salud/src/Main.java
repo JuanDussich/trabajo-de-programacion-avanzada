80d7ce674d1b60df65037478076262b6470ffdb8
JuanDussich <jmdussich@gmail.com>
2025-04-21 11:36:03 -0300

    prueba2

--- Centro de salud/src/Main.java
+++ Centro de salud/src/Main.java
@@ -6,8 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        JOptionPane.showMessageDialog(null,"hola soy juan");
-
+        JOptionPane.showMessageDialog(null,"hola soy juan en main");
 
     }
 }
