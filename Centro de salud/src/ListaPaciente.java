d7f12141fdec978abf4ffe3d91eb4e0628fb065a
Brailo01 <165711827+Brailo01@users.noreply.github.com>
2025-04-28 09:33:02 -0300

    Se actualiza ListaPaciente.

--- Centro de salud/src/ListaPaciente.java
+++ Centro de salud/src/ListaPaciente.java
@@ -1,7 +1,28 @@
 public class ListaPaciente {
 
-    //ATRIBUTOS
-    // CONSTRUCTOR
-    // METODOS
-    // GETTERS Y SETTERS
+	import java.util.ArrayList;
+
+	public class ListaPaciente {
+	    private ArrayList<Paciente> pacientes;
+
+	    public ListaPaciente() {
+	        this.pacientes = new ArrayList<>();
+	    }
+
+	    public void agregarPaciente(Paciente paciente) {
+	        pacientes.add(paciente);
+	    }
+
+	    public void eliminarPaciente(Paciente paciente) {
+	        pacientes.remove(paciente);
+	    }
+
+	    public ArrayList<Paciente> getPacientes() {
+	        return pacientes;
+	    }
+
+	    public void setPacientes(ArrayList<Paciente> pacientes) {
+	        this.pacientes = pacientes;
+	    }
+	}
 }
