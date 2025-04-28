a5ea3ed6f7c7e2849b55641bd1f3f1a7ea9c6863
JuanDussich <jmdussich@gmail.com>
2025-04-28 14:51:07 -0300

    Update Administrador.java

--- Centro de salud/src/Administrador.java
+++ Centro de salud/src/Administrador.java
@@ -11,12 +11,13 @@ public class Administrador {
             opcion = JOptionPane.showOptionDialog(null, "menu", "menu principal", JOptionPane.DEFAULT_OPTION, 0,  null,OpcionesAdministrador.values(), OpcionesAdministrador.values());
             switch (opcion){
                 case 0:
-
+                    JOptionPane.showMessageDialog(null, "aca vas a poder ver la informacion del administrador");
                     break;
                 case 1:
+                    JOptionPane.showMessageDialog(null, "aca vas a poder ver la lista de Medicos");
                     break;
                 case 2:
-
+                    JOptionPane.showMessageDialog(null, "aca vas a poder ver la lista de Pacientes");
                     break;
                 case 3:
                     JOptionPane.showMessageDialog(null, "a bueno adios master");
