4125a959f0213377d3d530994971ae8212160492
Brailo01 <165711827+Brailo01@users.noreply.github.com>
2025-04-28 09:32:32 -0300

    Se actualiza ListaMedico.

--- Centro de salud/src/ListaMedico.java
+++ Centro de salud/src/ListaMedico.java
@@ -1,7 +1,28 @@
 public class ListaMedico {
 
-    //ATRIBUTOS
-    // CONSTRUCTOR
-    // METODOS
-    // GETTERS Y SETTERS
+	import java.util.ArrayList;
+
+	public class ListaMedico {
+	    private ArrayList<Medico> medicos;
+
+	    public ListaMedico() {
+	        this.medicos = new ArrayList<>();
+	    }
+
+	    public void agregarMedico(Medico medico) {
+	        medicos.add(medico);
+	    }
+
+	    public void eliminarMedico(Medico medico) {
+	        medicos.remove(medico);
+	    }
+
+	    public ArrayList<Medico> getMedicos() {
+	        return medicos;
+	    }
+
+	    public void setMedicos(ArrayList<Medico> medicos) {
+	        this.medicos = medicos;
+	    }
+	}
 }
